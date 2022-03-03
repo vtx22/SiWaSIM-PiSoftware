6907eae41062b8143b88ae7f44ad5db3f73e90e5
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-03-03 16:26:57 +0100

    Update I2C.cpp

--- src/I2C.cpp
+++ src/I2C.cpp
@@ -7,10 +7,11 @@ I2C::I2C(std::string dev, uint16_t address) : _dev(dev), _address(address)
 bool I2C::begin()
 {
    _file = open(_dev, O_RDWR);
-   if (file < 0)
+   if (_file < 0)
    {
       printf("I2C ERROR: Cannot open device!");
       return false;
    }
    ioctl(_file, I2C_SLAVE, _address);
+   return true;
 }
