a20945eb01f1ebcf7e4848644a11258d0088cc1f
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-03-10 11:25:26 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <string>
+#include <signal.h>
+#include <pigpio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "I2C.hpp"
 #include "UART.hpp"
@@ -7,9 +11,13 @@
 int main()
 {
    //I2C test("/dev/ttyAMA0", 0x50);
-   UART com();
+   //UART com();
    //test.begin();
 
    //uint8_t message[10] = {0xFF, 0xFF, 0xFF, 0xAA, 0xAB};
    //com.transmitMSG(message, 10);
+
+   gpioInitialise();
+
+   gpioHardwarePWM(18, 10000, 50);
 }
