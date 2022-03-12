b9af9856015f7a159ab5a69e6af0330d1fdcd246
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-03-12 19:24:00 +0100

    Update GPIO.cpp

--- src/GPIO.cpp
+++ src/GPIO.cpp
@@ -15,6 +15,21 @@ GPIO::~GPIO()
 
 void GPIO::setPWM(int pin, float dutyCycle, float frequency)
 {
+   if (frequency < 1)
+   {
+      float period = 1 / frequency;
+      float highttime = period * dutyCycle / 100.f;
+   }
+
+   if (frequency == 995)
+   {
+      frequency--;
+   }
+   if (frequency == 996)
+   {
+      frequency++;
+   }
+
    if (gpioHardwarePWM(pin, frequency, 1e6 * dutyCycle / 100.f) < 0)
    {
       printf("PWM Error\n");
