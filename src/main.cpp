8bc61f0d2296e7056d12916ef6c67adec5c232d8
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-04-11 18:26:01 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,13 @@ int main()
    IABoard board;
 
    board.detectBoard();
-   time_sleep(0.01);
+   time_sleep(0.001);
    board.setOpenDrainDOUT(1, 1);
-   time_sleep(0.01);
+   time_sleep(0.001);
    board.setOpenDrainDOUT(2, 1);
-   time_sleep(0.01);
+   time_sleep(0.001);
    board.setOpenDrainDOUT(3, 1);
-   time_sleep(0.01);
+   time_sleep(0.001);
    board.setOpenDrainDOUT(4, 1);
 
    /*
