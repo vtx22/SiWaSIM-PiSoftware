19d977cd6ab6c66b03db95b2fe826c9dc67d474f
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-04-19 14:09:38 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,23 +8,11 @@
 #include "UART.hpp"
 #include "GPIO.hpp"
 #include "IABoard.hpp"
+#include "PCB.hpp"
 
 int main()
 {
-   UART uart;
+   PCB pcb;
 
-   uint8_t data[3] = {0xFF, 0xEE, 0xAD};
-
-   uart.transmitMSG(data, 3);
-
-   printf("Waiting for message...\n");
-
-   std::vector<uint8_t> received = uart.receiveMSG();
-
-   printf("RECEIVED %d BYTES!\n", received.size());
-
-   for (int i = 0; i < received.size(); i++)
-   {
-      printf(" %d ", received[i]);
-   }
+   pcb.ledBusy(1);
 }
