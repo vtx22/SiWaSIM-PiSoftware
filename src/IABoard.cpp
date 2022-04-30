e31d547c8cb5decb8b1f8b5c4428909e57f5313b
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-05-01 01:14:16 +0200

    Update IABoard.cpp

--- src/IABoard.cpp
+++ src/IABoard.cpp
@@ -254,7 +254,7 @@ void IABoard::setLED(uint8_t channel, bool value)
       printf("IA-Board ERROR: Channel out of range! Allowed: 1 - 4\n");
       return;
    }
-   std::this_thread::sleep_for(100us);
+   std::this_thread::sleep_for(1ms);
    uint8_t cmd = 0x02;
    if (value)
    {
