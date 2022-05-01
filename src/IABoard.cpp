0d9324796c3e9fc5962b59e7c895251d7e549db4
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-05-01 02:11:18 +0200

    Update IABoard.cpp

--- src/IABoard.cpp
+++ src/IABoard.cpp
@@ -339,7 +339,7 @@ void IABoard::waitForIA()
    if (diff < _delayBetweenCommands.count())
    {
       std::cout << "Difference too small....waiting....\n";
-      std::this_thread::sleep_for(std::chrono::milliseconds(std::chrono::duration_cast<std::chrono::milliseconds>(_delayBetweenCommands - diff).count()));
+      std::this_thread::sleep_for(std::chrono::milliseconds(_delayBetweenCommands.count() - diff);
    }
 
    _lastCommand = std::chrono::system_clock::now();
