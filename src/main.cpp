d5227e07caf4397ce2f4c1566a67ba610f5c23d9
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-05-08 00:02:49 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -21,8 +21,10 @@ int main()
 
    sim.bootupAnimation();
    std::vector<float> data, x;
+   data.push_back(0);
+   x.push_back(0);
 
-   for (uint8_t i = 0; i < 50; i++)
+   for (uint8_t i = 1; i < 51; i++)
    {
       x.push_back(i / 10.f);
 
