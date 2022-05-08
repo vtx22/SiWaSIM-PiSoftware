9f4b6d0b532d0018fba11732e912d24a9cf1096d
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-05-08 03:09:55 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -25,29 +25,17 @@ int main()
    weightData.push_back(0);
    x.push_back(-0.1);
 
-   plt::figure_size(1600, 900);
    for (uint8_t i = 0; i < 50; i++)
    {
       x.push_back(i / 10.f);
 
       data.push_back(sim.run(RUN_MODE::PASSIVE, 0.1, &weight));
       weightData.push_back(weight);
-      if (i % 10 == 0)
-      {
-         plt::clf();
 
-         plt::plot(x, data, {{"color", "b"}, {"label", "flow in kg/s"}});
-         plt::plot(x, weightData, {{"color", "r"}, {"label", "weight in kg"}});
-
-         plt::title("First flow test, step time 100ms");
-         plt::xlabel("Time in seconds");
-         plt::legend();
-         plt::grid(true);
-         plt::pause(0.01);
-      }
       std::this_thread::sleep_for(100ms);
    }
-   plt::clf();
+
+   plt::figure_size(1600, 900);
 
    plt::plot(x, data, {{"color", "b"}, {"label", "flow in kg/s"}});
    plt::plot(x, weightData, {{"color", "r"}, {"label", "weight in kg"}});
