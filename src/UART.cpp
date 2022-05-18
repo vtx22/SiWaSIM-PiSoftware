992c8dd1d5db67bb33ba7bb541f671f176d5c117
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-05-18 17:25:38 +0200

    Update UART.cpp

--- src/UART.cpp
+++ src/UART.cpp
@@ -23,7 +23,7 @@ bool UART::begin()
    struct termios options;
    tcgetattr(uart0, &options);
 
-   options.c_cflag = B19200 | CS8 | CLOCAL | CREAD | HUPCL | PARENB;
+   options.c_cflag = B19200 | CS8 | CLOCAL | CREAD | HUPCL | PARENB | INPCK;
    // options.c_cflag |= PARENB;
    options.c_iflag = 0;
    options.c_oflag = 0;
