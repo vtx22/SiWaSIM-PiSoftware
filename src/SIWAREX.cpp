67f9ecee1deaa7a82cc3ac279c8314a214705e4d
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-05-19 14:43:31 +0200

    Update SIWAREX.cpp

--- src/SIWAREX.cpp
+++ src/SIWAREX.cpp
@@ -15,12 +15,5 @@ float SIWAREX::getLoadcellVoltage()
    _modbus->transmitRequest(LOADCELL_VOLTAGE, 2);
    std::vector<uint8_t> msg = _modbus->receiveResponse();
 
-   printf("\n\n");
-   for (auto const &i : msg)
-   {
-      printf(" %d ", i);
-   }
-   printf("\n\n");
-
    return bytesToFloat(msg[2], msg[3], msg[4], msg[5]);
 }
