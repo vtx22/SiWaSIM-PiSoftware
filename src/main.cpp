7fd38c74d8f3fbd888aaf1b3063106a4413c9525
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-05-19 16:57:19 +0200

    aa

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ int main()
    SIWAREX siwarex;
    std::vector<float> data, x;
    int cnt = 0;
-   while (cnt < 60 * 30)
+   while (cnt < 60 * 0.5)
    {
       float voltage = siwarex.getLoadcellVoltage();
       printf("Voltage is: %f mV\n", voltage);
@@ -42,6 +42,7 @@ int main()
    plt::title("Load Cell Voltage with analog bridge");
    plt::xlabel("Time in seconds");
    plt::legend();
+   plt::ylim(19.2, 20.8);
    plt::grid(true);
    plt::show();
 
