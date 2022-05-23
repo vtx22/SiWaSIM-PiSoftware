3510cc2b81c36f3807bbf3f01b511c77a74aca0b
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-05-24 01:55:24 +0200

    Update SIWAREX.cpp

--- src/SIWAREX.cpp
+++ src/SIWAREX.cpp
@@ -29,15 +29,18 @@ float SIWAREX::requestFloat(uint16_t startRegister)
 
 std::vector<uint8_t> SIWAREX::requestRegisters(uint16_t startRegister, uint16_t length)
 {
+   printf("Requesting %d registers from %d\n", length, startRegister);
    _modbus->transmitRequest(startRegister, length);
    std::vector<uint8_t> msg = _modbus->receiveResponse();
 
+   printf("Received %d bytes\n", msg.size());
    msg.erase(msg.size() - 1);
    msg.erase(msg.size() - 1);
 
    msg.erase(msg.begin());
    msg.erase(msg.begin());
    msg.erase(msg.begin());
+   printf("After clearing: %d bytes\n", msg.size());
 
    return msg;
 }
