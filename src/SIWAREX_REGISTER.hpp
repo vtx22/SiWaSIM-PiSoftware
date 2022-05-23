e18e2032e93ebb58332c3f7de44473c363585316
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-05-23 23:38:00 +0200

    Update SIWAREX_REGISTER.hpp

--- src/SIWAREX_REGISTER.hpp
+++ src/SIWAREX_REGISTER.hpp
@@ -13,7 +13,7 @@ struct DATASET
 {
    uint16_t startRegister;
    uint8_t length;
-   std::vector<PARAMS> params;
+   // std::vector<PARAMS> params;
 } typedef DATASET;
 
 #define LOADCELL_IMPEDANCE 3056
