4f733f2806778159a55b5ad9f1e292b9cdb5ec2e
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-05-25 12:24:33 +0200

    Update Simulator.cpp

--- src/Simulator.cpp
+++ src/Simulator.cpp
@@ -214,7 +214,7 @@ std::vector<float> Simulator::longTermTest()
    std::this_thread::sleep_for(5s);
    std::vector<float> yValues;
 
-   int numOfSamples = 60 * 60 * 9;
+   const int numOfSamples = 60 * 15;
    int sample = 0;
    bool state = false;
 
