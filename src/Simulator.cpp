41e846e357668d05979e12100be30100bedf0639
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-06-08 04:14:21 +0200

    Update Simulator.cpp

--- src/Simulator.cpp
+++ src/Simulator.cpp
@@ -148,6 +148,8 @@ void Simulator::testFunction()
    _siwarex->setMODBUSDelay(0);
    std::this_thread::sleep_for(1s);
    printf("DELAY: %d\n", _siwarex->getMODBUSDelay());
+   std::this_thread::sleep_for(1s);
+   printf("VOL: %f\n", _siwarex->getLoadcellVoltage());
    return;
    _materialFlows[0]->calculateFlowCurve();
    while (true)
