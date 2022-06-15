5c209b2e46138da4de6e7b481bf53869274d35b8
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-06-15 03:54:18 +0200

    Update SIWAREX.cpp

--- src/SIWAREX.cpp
+++ src/SIWAREX.cpp
@@ -89,13 +89,14 @@ MODBUS_PARAMETER SIWAREX::getParameter(MODBUS_PARAMETER param)
 
 void SIWAREX::writeRegister(uint8_t dataset, uint16_t startRegister, uint16_t value)
 {
-   //_modbus->writeRegister(CMD1_CODE, 2000 + dataset);
+   // Enable Service Mode
    _modbus->writeRegisters(CMD1_CODE, std::vector<uint16_t>{0x01, 0x01});
-   return;
+   // Load Dataset
    _modbus->writeRegisters(CMD1_CODE, std::vector<uint16_t>{2000 + dataset, 0x01});
+   // Change register
    _modbus->writeRegister(startRegister, value);
+   // Save Dataset
    _modbus->writeRegisters(CMD1_CODE, std::vector<uint16_t>{4000 + dataset, 0x01});
-   _modbus->writeRegister(CMD1_CODE, 0x02);
-   //_modbus->writeRegister(CMD1_CODE, 4000 + dataset);
-   //_modbus->writeRegister(CMD1_TRIGGER, 0x01);
+   // Disable Service Mode
+   _modbus->writeRegisters(CMD1_CODE, std::vector<uint16_t>{0x02, 0x01});
 }
